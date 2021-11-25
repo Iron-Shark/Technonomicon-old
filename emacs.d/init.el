dab1f11647738128c93f510689a40e8ce4f494bb
Que <git@ironshark.org>
2021-11-25 15:15:22 -0500

    added settings to emacs init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,3 +6,25 @@
 (set-fringe-mode 10)	;Give some breathing room
 
 (setq visible-bell t)	;Enables visual bell
+
+;;TODO update default font height and type
+(load-theme 'tango-dark)
+
+;;Initialize Package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;(unless (package-installed-p 'use-package)
+;  (package-refresh-contents)
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
