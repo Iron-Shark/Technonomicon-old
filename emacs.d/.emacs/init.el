9929e9ab27b135630097372cb55c403b47ca3de5
Que <git@ironshark.org>
2021-11-25 14:10:17 -0500

    removed unneed extra directory

--- emacs.d/.emacs/init.el
+++
@@ -1,8 +0,0 @@
-(setq inhibit-startup-message t)	;Disabls start screen message
-
-(scroll-bar-mode -1)	;Disable visible scroll bar
-(tool-bar-mode -1)	;Disable the toolbar
-(tooltip-mode -1)	;Disable tooltips
-(set-fringe-mode 10)	;Give some breathing room
-
-(setq visible-bell t)	;Enables visual bell
