7184e1bf231ffa6fd3522af7179f43dcc5fff98e
Que <git@ironshark.org>
2021-11-26 09:48:06 -0500

    added font, and made changes to emacs init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,21 @@
-(setq inhibit-startup-message t)	;Disabls start screen message
+;;Configure use package and repositories
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package) ;;Requires use-package on startup
+(setq use-package-always-ensure t)
+
+;;Basic apperance configuration
+(setq inhibit-startup-message t)	;Disables start screen message
 
 (scroll-bar-mode -1)	;Disable visible scroll bar
 (tool-bar-mode -1)	;Disable the toolbar
@@ -7,24 +24,15 @@
 
 (setq visible-bell t)	;Enables visual bell
 
-;;TODO update default font height and type
-(load-theme 'tango-dark)
+;;Create default font size
+(defvar runemacs/default-font-size 180)
+;;Configure font
+(set-face-attribute 'default nil :font "Overpass" :height runemacs/default-font-size)
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Overpass" :height 260)
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 295 :weight 'regular)
 
-;;Initialize Package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
- (package-refresh-contents))
-
-;(unless (package-installed-p 'use-package)
-;  (package-refresh-contents)
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
+;;Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
--- guix/manifest.scm
+++ guix/manifest.scm
@@ -1,7 +1,10 @@
 (specifications->manifest
-      '("emacs-native-comp"
-	"vim"
-	"git"
-	"lxde"
-	"firefox"
+      '("emacs-native-comp"	;Emacs text editor locally compilled
+	"vim"		;Vim text editor
+	"git"		;Git version control
+	"lxde"		;LXDE Display manager
+	"firefox"	;Firefox nightly, locally compiled
+	"font-overpass" ;Overpass font theme - https://overpassfont.org/
 	))
+
+;;after updating run "guix package --manifest=.dotfiles/guix/manifest.scm"
