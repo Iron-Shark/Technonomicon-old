dfee67607cca70fc466420c5c89300bcde315f23
Que <git@ironshark.org>
2021-11-26 13:06:08 -0500

    added unclutter, emacs-guix,  nix&flatpak

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -5,6 +5,10 @@
 	"lxde"		;LXDE Display manager
 	"firefox"	;Firefox nightly, locally compiled
 	"font-overpass" ;Overpass font theme - https://overpassfont.org/
+	"nix"		;NIX package manager
+	"flatpak"	;Flatpak package manager
+	"emacs-guix"	;Guix pkm interface for emacs
+	"unclutter"	;Hide cursor when mouse is inactive
 	))
 
 ;;after updating run "guix package --manifest=.dotfiles/guix/manifest.scm"
