79480ae06fbfb11d638336894548ae25c957c804
Que <Git@ironshark.org>
2021-12-16 08:44:09 -0500

    updating emacs and system config setting. Broke Nix may rebuild

--- guix/channels.scm
+++ guix/channels.scm
@@ -24,3 +24,5 @@
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
           "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+
+;; After making changes to this file run '$ guix pull --channels=path/to/this/file"
