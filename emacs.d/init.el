70ee574e6b5072a680607bbd05b1215d6c9034a7
Que <git@ironshark.org>
2022-01-23 16:12:03 -0500

    changed a bunch of stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,23 +3,25 @@
 
 ;; Basic UI Configuration ----------------------------------------------------------------
 ;; You will most likely need to adjust this font size for your system!
-(defvar runemacs/default-font-size 125)
+(defvar runemacs/default-font-size 150)
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)        	; Disable visible scrollbar
 (tool-bar-mode -1)          	; Disable the toolbar
 (tooltip-mode -1)           	; Disable tooltips
 (set-fringe-mode 5)        	; Give some breathing room
 (menu-bar-mode -1)            	; Disable the menu bar
-
 (column-number-mode)
 (global-display-line-numbers-mode t)
 (global-visual-line-mode t)
+(show-paren-mode t)
+(setq-default fill-column 80)
+
 
 ;; Set up the visible bell
 (setq visible-bell t)
 
 ;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
+(dolist (mode '(pdf-view-mode-hook
 		term-mode-hook
                 shell-mode-hook
                 eshell-mode-hook))
@@ -28,13 +30,13 @@
 ;; Start Emacs in fullscreen mode
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+;; Full Screen Frame
+(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 
 ;; Set Location Data----------------------------------------------------------------------
 (setq calendar-latitude 42.33)
 (setq calendar-longitude -83.04)
 (setq calendar-location-name "Detroit,MI")
-(column-number-mode)
-(global-display-line-numbers-mode t)
 
 ;; Package Management---------------------------------------------------------------------
 ;; Detirmines if straight.el is present.
@@ -70,33 +72,15 @@
 (setq use-package-always-ensure t)
 
 ;; System Dependencies------------------------------------------------------------------
-;; Async
-(use-package async
-  :straight (:host github 
-		   :repo "jwiegley/emacs-async"
-		   :branch "master"))
-;; Popup
-(use-package popup
-  :straight (:host github 
-		   :repo "auto-complete/popup-el"
-		   :branch "master"))
 ;; Emojify 
-(use-package emojify
-  :straight (:host github 
-		   :repo "iqbalansari/emacs-emojify"
-		   :branch "master"))
+(use-package emojify)
+
 ;; Undo Tree
-(use-package undo-tree
-  :straight (:host github 
-		   :repo "emacs-straight/undo-tree"
-		   :branch "master"))
+(use-package undo-tree)
+(global-undo-tree-mode 1)
 
 ;; Which-key
 (use-package which-key
-  :straight (:host github
-		   :repo "justbur/emacs-which-key" 
-		   :branch "master")
-	     	   :defer 0
   		   :diminish which-key-mode
   		   :config
   		   (which-key-mode)
@@ -104,111 +88,49 @@
 
 ;; Helpful
 (use-package helpful 
-  :straight (:host github 
-		   :repo "Wilfred/helpful"
-		   :branch "master")
                	   :commands (helpful-callable helpful-variable helpful-command helpful-key)
-                   :custom
-                   (counsel-describe-function-function #'helpful-callable)
-                   (counsel-describe-variable-function #'helpful-variable)
                    :bind
                    ([remap describe-command] . helpful-command)
                    ([remap describe-key] . helpful-key))
 
 ;; Icons
-(use-package all-the-icons
-  :straight (:host github
-		   :repo "domtronn/all-the-icons.el"
-		   :branch "master"))
+(use-package all-the-icons)
 ;; After downloading run "M-x all-the-icons-install-fonts" on first start up.
 
 ;; Visual Changes-----------------------------------------------------------------------
 ;; Font Configuration
 (set-face-attribute 'default nil :font "JetBrains Mono"
                        		 :weight 'semibold
-				 :height 160)
+				 :height 180)
 ;; Set the fixed pitch face
 (set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" 
 		    		     :weight 'semibold 
-				     :height 160)
+				     :height 180)
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Overpass" 
-					:weight 'regular)
-		    			:height 160 
+					:weight 'regular
+		    			:height 220) 
 
 ;; Doom Themes
 (use-package doom-themes
-  :straight (:host github
-		   :repo "hlissner/emacs-doom-themes"
-		   :branch "master")
   :init (load-theme 'doom-city-lights t))
 		   
 ;; Doom Modline
 (use-package doom-modeline
-  :straight (:host github
-		   :reop "seagle0128/doom-modeline"
-		   :branch "master")
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
-;; Helm----------------------------------------------------------------------------------
-(use-package helm 
-  :straight (:host github
-		  :repo "emacs-helm/helm"
-		  :branch "master"))
-	         ;:bind
-
-;; Set Global Helm Keys
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(global-set-key (kbd "C-c h") #'helm-command-prefix)
-
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t
-      helm-echo-input-in-header-line t)
-
-(defun runemacs//helm-hide-minibuffer-maybe ()
-  "Hide minibuffer in Helm session if we use the header line as input field."
-  (when (with-helm-buffer helm-echo-input-in-header-line)
-    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-      (overlay-put ov 'window (selected-window))
-      (overlay-put ov 'face
-                   (let ((bg-color (face-background 'default nil)))
-                     `(:background ,bg-color :foreground ,bg-color)))
-      (setq-local cursor-type nil))))
-
-
-(add-hook 'helm-minibuffer-set-up-hook
-          'runemacs//helm-hide-minibuffer-maybe)
-
-(setq helm-autoresize-max-height 0)
-(setq helm-autoresize-min-height 20)
-(helm-autoresize-mode 1)
-
-
-(helm-mode 1)
+(use-package rainbow-delimiters
+  :init (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 ;; Keyboard ------------------------------------------------------------------------------------
 ;; Assign ESC to quite prompts
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-S-v") 'clipboard-yank)
 
 ;; Evil
 (use-package evil
-  :straight (:host github 
-		   :repo "emacs-evil/evil"
-		   :branch "master")
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
@@ -222,35 +144,24 @@
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
 
 ;; Evil Collection
-(use-package evil-collection
-  :straight (:host github 
-		   :repo "emacs-evil/evil-collection"
-		   :branch "master"))
+(use-package evil-collection)
 
 ;; General
 (use-package general
-  :straight (:host github 
-		   :repo "noctuid/general.el"
-		   :branch "master")
   :after evil
   :config
-  (general-create-definer efs/leader-keys
+  (general-create-definer runemacs/leader-keys
   :keymaps '(normal insert visual emacs)
   :prefix "SPC"
   :global-prefix "C-SPC")
 
-  (efs/leader-keys
+  (runemacs/leader-keys
    "t"  '(:ignore t :which-key "toggles")))
 
 ;; Set Emacs state modes
 (dolist (mode '(custom-mode
                 eshell-mode
                 git-rebase-mode
-                erc-mode
-                circe-server-mode
-                circe-chat-mode
-                circe-query-mode
-                sauron-mode
                 term-mode))
         (add-to-list 'evil-emacs-state-modes mode))
 
@@ -264,19 +175,27 @@
 (evil-set-initial-state 'messages-buffer-mode 'normal)
 (evil-set-initial-state 'dashboard-mode 'normal)
 
-;; Org Submodes---------------------------------------------------------------------------
-;; Org
-(use-package org
-  :straight (:host github 
-		   :repo "bzg/org-mode"
-		   :branch "main")
-  :commands (org-capture org-agenda)
-; :hook (org-mode . efs/org-mode-setup)
-  :config
-   (setq org-ellipsis " ▾"))
+;; Fly-X----------------------------------------------------------------------------------
+;; FlySpell
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda ()
+	(setq ispell-program-name "~/.guix-profile/bin/hunspell")
+	(flyspell-mode 1))))
+;;TODO Install Flycheck
 
-;; Org Font Config
-(defun efs/org-font-setup ()
+;; Org------------------------------------------------------------------------------------
+;; Turn on indentation and auto-fill mode for Org files
+(defun runemacs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil)
+  (display-line-numbers-mode 0)
+  (diminish org-indent-mode))
+
+
+(defun runemacs/org-font-setup ()
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
                           '(("^ *\\([-]\\) "
@@ -291,7 +210,7 @@
                   (org-level-6 . 1.1)
                   (org-level-7 . 1.1)
                   (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+    (set-face-attribute (car face) nil :font "Overpass" :weight 'regular :height (cdr face)))
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
@@ -306,31 +225,60 @@
   (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
   (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
 
-(defun efs/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-;; Org Roam
-(use-package org-roam
-  :straight (:host github 
-	     :repo "org-roam/org-roam"
-             :files (:defaults "extensions/*"))
-  :init (setq org-roam-v2-ack t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-	 ("C-c n f" . org-roam-node-find)
-	 ("C-c n i" . org-roam-node-insert)
-	 ("C-c n t" . org-roam-tag-add))
+(use-package org
+  :hook (org-mode . runemacs/org-mode-setup)
+        (org-mode . runemacs/org-font-setup)
   :config
-  (org-roam-setup)
-  (org-roam-db-autosync-mode))
-(setq org-roam-directory
-      (file-truename "~/Archive/Wiki/Nodes"))
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t	
+        org-src-fontify-natively t
+        org-fontify-quote-and-verse-blocks t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 2
+        org-hide-block-startup nil
+        org-src-preserve-indentation nil
+        org-startup-folded 'content
+        org-cycle-separator-lines 2
+        org-capture-bookmark nil)
 
-;; Fly-X----------------------------------------------------------------------------------
-;; FlySpell
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda ()
-	(setq ispell-program-name "~/.guix-profile/bin/hunspell")
-	(flyspell-mode 1))))
-;;TODO Install Flycheck
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-j") 'org-next-visible-heading)
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-k") 'org-previous-visible-heading)
+
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-j") 'org-metadown)
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-k") 'org-metaup)
+
+  (org-babel-do-load-languages
+    'org-babel-load-languages
+    '((emacs-lisp . t)
+      (ledger . t)
+      (lisp . t)
+      (latex . t)
+      (scheme . t)))
+  
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+
+(defun runemacs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . runemacs/org-mode-visual-fill))
+
+;; Latex Mode
+(use-package tex
+  :straight auctex)
+;; Changes default render engine.
+(setq latex-run-command "xelatex")
+(setq org-latex-compiler "xelatex")
+
+(use-package latex-preview-pane)
+
+;; PDF
--- guix.bak/manifest.scm
+++ guix.bak/manifest.scm
@@ -1,21 +1,30 @@
 (specifications->manifest
-      '("emacs-native-comp"	;Emacs text editor locally compilled
-	"vim"			;Vim text editor
-	"git"			;Git version control
+      '("vim"			;Vim text editor
 	"firefox"		;Firefox nightly, locally compiled
-	;"nix"			;Nix packagemanager
+	"unclutter"		;Hide cursor when mouse is inactive
+	"plover"
+	;;
+	;; System Tools
+	"git"			;Git version control
+	"gcc-toolchain" 	;C/C++ compiler tools.
+	"clang"         	;C/C++ compiler front end.
+	"stow"			;Symlink manager
+	"curl"			;Terminal URL download tool
+	;;
+	;; Emacs 
+	"emacs-native-comp"	;Emacs text editor locally compilled
+	   ;; Emacs Package Dependencies
 	"font-overpass" 	;Overpass font theme - https://overpassfont.org/
 	"font-jetbrains-mono" 	;JetBrains font theme - https://www.jetbrains.com/
 	"fontconfig"		;After installing this and target fonts run "$fc-cache -rv"
 	"hunspell"		;Hunspell spellchecker backend
 	"hunspell-dict-en-us"   ;US English dictionary for hunspell
-	"unclutter"		;Hide cursor when mouse is inactive
-	"gcc-toolchain" 	;C/C++ compiler tools.
-	"clang"         	;C/C++ compiler front end.
-	"stow"			;Symlink manager
-	"curl"			;Terminal URL download tool
+	;; Testing
+	"haunt"
+	"nyxt"
+	"texlive"		;LaTeX engine
+	"texlive-latex-preview" ;LaTeX live preview
 	))
 
 ;;after updating run "guix package --manifest=.dotfiles/guix/manifest.scm"
 ;
-;; Nix PKM installed using bash <(curl -L https://nixos.org/nix/install) --no-deamon
