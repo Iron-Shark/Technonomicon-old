429482b7e398c4062a1d9ec204d98bd314629ffc
Que <git@ironshark.org>
2022-01-24 18:25:17 -0800

    Added pdf workflow tools. Still configuring

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -328,3 +328,38 @@
   (add-to-list 'org-file-apps
 	       '("\\.pdf\\'" . (lambda (file link)
 				 (org-pdfview-open link)))))
+
+;; PDF to Org-Mode Tools
+(use-package org-noter)
+
+(use-package org-pdftools
+  :hook (org-mode . org-pdftools-setup-link))
+
+(use-package org-noter-pdftools
+  :after org-noter
+  :config
+  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
+						   (not org-noter-insert-note-no-questions)
+						 org-noter-insert-note-no-questions))
+	   (org-pdftools-use-isearch-link t)
+	   (org-pdftools-use-freestyle-annot t))
+       (org-noter-insert-note (org-noter--get-precise-info)))))
+  (defun org-noter-set-start-location (&optional arg)
+    "When opening a session with this document, go to the current location. With a prefix ARG, remove start location."
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((inhibit-read-only t)
+	   (ast (org-noter--parse-root))
+	   (location (org-noter--doc-approx-location (when (called-interactively-p 'any) 'interactive))))
+       (with-current-buffer (org-noter--session-notes-buffer session)
+	 (org-with-wide-buffer)
+	 (goto-char (org-element-property :begin ast))
+	 (if arg
+	     (org-entry-delete nil org-noter-property-note-location)
+	   (org-entry-put nil org-noter-property-note-location
+			  (org-noter--pretty-print-location location)))))))
+  (with-eval-after-load 'pdf-annot
+    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
